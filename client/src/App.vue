219389beb6dc1bfc9454f3be82fc44aac50b3391
radenlmantuano <newcodeondablock@gmail.com>
2022-01-31 14:10:11 -0800

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Home />
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Roboto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,38 @@
+
+import block from '../assets/block_1404_test.png'
+<template>
+  <div>
+    <h1>HELLO WORLD! <br><br>THIS IS THE VERY BEGINNING OF THE VERY FIRST ETH MUSEUM</h1>
+    <img src='../assets/block_1404_test.png' />
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+  font-size:  1.714rem;
+  font-weight: 400;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
